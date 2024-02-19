1b96f8714706eb402febfe6548b525c3e92f070a
Ivan Rainbolt <IvanRainbolt@outlook.com>
2024-02-19 13:42:08 -0500

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+
+
+Application app = Application.Current ?? AppBuilder.Configure<Application>().UsePlatformDetect().SetupWithoutStarting().Instance;
+app.Styles.Add(new DefaultTheme());
+app.Run(new Window() { Title = "Avalonia Basic Example", Content = "Hello Avalonia!" });
